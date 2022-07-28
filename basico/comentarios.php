8e73994ecba2bf54dce1061f0959f9397d44efe8
RD-Ricardo <ricardodias169@gmail.com>
2022-07-28 10:56:11 -0300

    conhecendo o PHP

---
+++ basico/comentarios.php
@@ -0,0 +1,20 @@
+<div class="titulo">Comentários</div>
+
+
+
+<?php 
+    // echo "php";
+    #echo "comentario"; 
+    
+//?>
+
+<?php 
+    /*
+        echo "comentário de multiplas linhas";
+    */
+?>
+
+<!--
+    Comentario HTML 
+    <p>Depois do primeiro bloco</p> 
+-->
---
+++ exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudos-PHP</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/exercicio.css">
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do ecercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatacao</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            <?php
+                include("{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
+        </div>
+    </main>
+    <footer  class="rodape">
+        RD-Ricardo <?= date('Y');?>
+    </footer>
+</body>
+</html>
